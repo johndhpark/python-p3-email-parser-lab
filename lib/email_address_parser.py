94a81506089c7528ee6c1dd9358b313c00663f6c
John DH. Park <john.dh.park@icloud.com>
2023-08-08 20:52:31 -0700

    Completed Email Parser Lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,25 @@
-# your code goes here!
+import re
+import ipdb
+
+class EmailAddressParser():
+	def __init__(self, str):
+		self.str = str
+
+	def parse(self):
+		split_pat = re.compile(r'\s|,\s*')
+
+		words = split_pat.split(self.str)
+
+		email_pat = re.compile(r'[A-z0-9._%+-]+@[A-z0-9.-]+\.[A-z]{2,}')
+
+		emails = []
+
+
+
+		for word in words:
+			if email_pat.match(word):
+				emails.append(word)
+
+		emails.sort()
+
+		return emails
